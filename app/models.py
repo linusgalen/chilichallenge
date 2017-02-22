f67ac16c68b0adeb9aa202e09be8904dcea5666d
jakobsanne <jakob.sanne@gmail.com>
2017-02-22 16:22:14 +0100

    Update on the database structure, relationships improved. Challenges table is now working, TODO is Order table and maybe make sure the other relationships are solid

--- app/models.py
+++ app/models.py
@@ -1,6 +1,17 @@
 from app import db
 
+# At first it looked better to do the "declarative mapping" according to alchemy
+# tutorial (then we can skip db. all the time but that needs some fixing in
+# the other files so right now it's back to normal.
+#
 
+# declare mappings as in http://docs.sqlalchemy.org/en/latest/orm/tutorial.html
+#from sqlalchemy.ext.declarative import declarative_base
+#from sqlalchemy import Column, Integer, String, ForeignKey
+#from sqlalchemy.orm import relationship
+#from sqlalchemy.types import DateTime
+
+#Base = declarative_base()
 
 """
 This file has been automatically generated with workbench_alchemy v0.2.3
@@ -9,6 +20,7 @@ https://github.com/PiTiLeZarD/workbench_alchemy
 """
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)  # pylint: disable=invalid-name
     email = db.Column(db.String(45), unique=True, nullable=False)
     password = db.Column(db.String(45), nullable=False)
@@ -16,9 +28,12 @@ class User(db.Model):
     address_id = db.Column(
         db.Integer, db.ForeignKey('address.id', onupdate="CASCADE", ondelete="CASCADE")
     )
-    address = db.relationship('Address', backref=db.backref('children', lazy='dynamic'))
-
-
+    # User-Address One-to-One Relationship
+    address = db.relationship('Address', uselist=False, back_populates ='user')
+    # adding stuff http://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html
+    # User-Challenge One-to-Many Relationship
+    challenge = db.relationship('Challenge')
+    order = db.relationship("Order", uselist=False, back_populates="user")
 
     @property
     def is_authenticated(self):
@@ -35,7 +50,6 @@ class User(db.Model):
     def get_id(self):
         return str(self.id)  # python 3
 
-    #addres = relationship("Addre", foreign_keys=[address_id], backref="user")
 
     def __repr__(self):
         return self.__str__()
@@ -45,43 +59,45 @@ class User(db.Model):
 
 
 class Product(db.Model):
-
+    __tablename__ = 'product'
     id = db.Column(db.Integer, autoincrement=True, primary_key=True)  # pylint: disable=invalid-name
     name = db.Column(db.String(45))
     description = db.Column(db.String(200))
     imgurl = db.Column(db.String(45))
     price = db.Column(db.String(45))
 
+    #One-to-Many relationship
+    challenge = db.relationship("Challenge")
+
     def __repr__(self):
         return str(self.id)
-
-
+#
+#   TODO: The order table is still not working properly.
+#   Should have a Many-to-one relationship with user and
+#   One-to-one with challenge.
 
 class Order(db.Model):
-    id = db.Column(db.Integer, autoincrement=False, primary_key=True, nullable=False)  # pylint: disable=invalid-name
+    __tablename__ = 'order'
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)  # pylint: disable=invalid-name
     datetime = db.Column(db.DateTime)
     user_id = db.Column(
-        db.Integer, db.ForeignKey("user.id", onupdate="CASCADE", ondelete="CASCADE"), autoincrement=False, primary_key=True,
-        index=True
+        db.Integer, db.ForeignKey('user.id', onupdate="CASCADE", ondelete="CASCADE"),
     )
     challenge_id = db.Column(
-        db.Integer, db.ForeignKey("challenge.id", onupdate="CASCADE", ondelete="CASCADE"), autoincrement=False,
-        primary_key=True, index=True
+        db.Integer, db.ForeignKey('challenge.id', onupdate="CASCADE", ondelete="CASCADE"),
     )
 
-    #challenge = relationship("Challenge", foreign_keys=[challenge_id], backref="order")
-    #user = relationship("User", foreign_keys=[user_id], backref="order")
+    challenge = db.relationship("Challenge", back_populates="order")
+    user = db.relationship("User", back_populates="order")
 
     def __repr__(self):
         return str(self.challenge_id)
 
 
-
-
 class Challenge(db.Model):
 
-
-    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
+    __tablename__ = 'challenge'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     message = db.Column(db.String(300))
     datetime = db.Column(db.DateTime)
     product_id = db.Column(
@@ -94,24 +110,21 @@ class Challenge(db.Model):
         db.Integer, db.ForeignKey("address.id", onupdate="CASCADE", ondelete="CASCADE")
     )
 
-
-    user = db.relationship('User', backref=db.backref('challenge', lazy='dynamic'))
-    address = db.relationship('Address', backref=db.backref('challenge', lazy='dynamic'))
-    product = db.relationship('Product', backref=db.backref('challenge', lazy='dynamic'))
-
-
-
-    #addres = relationship("Addre", foreign_keys=[address_id], backref="challenge")
-    #user = relationship("User", foreign_keys=[user_id], backref="challenge")
-    #product = relationship("Product", foreign_keys=[product_id], backref="challenge")
+    address = db.relationship("Address", back_populates="challenge")
+    user = db.relationship("User", back_populates="challenge")
+    product = db.relationship("Product", back_populates="challenge")
+    order = db.relationship("Order")
 
     def __repr__(self):
         return str(self.id)
 
-
+# Still not sure about this table below, check out: Many To Many @
+# http://docs.sqlalchemy.org/en/latest/orm/basic_relationships.html
 
 class UserHasUser(db.Model):
+    __tablename__ = 'user_has_user'
 
+    #id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id1 = db.Column(
         db.Integer, db.ForeignKey("user.id", onupdate="CASCADE", ondelete="CASCADE"), autoincrement=False, index=True,
         primary_key=True, nullable=False
@@ -121,18 +134,15 @@ class UserHasUser(db.Model):
         primary_key=True, nullable=False
     )
 
-    #user = relationship("User", foreign_keys=[user_id1], backref="userHasUser")
-    #user = relationship("User", foreign_keys=[user_id2], backref="userHasUser")
+    #user = db.relationship("User", backref="userHasUser")
+    #user = db.relationship("User", backref="userHasUser")
 
     def __repr__(self):
         return '1:'+str(self.user_id1)+'2:'+str(self.user_id2)
 
 
-
-
-
 class Address(db.Model):
-
+    __tablename__ = 'address'
     id = db.Column(db.Integer, primary_key=True)  # pylint: disable=invalid-name
     first_name = db.Column(db.String(45))
     last_name = db.Column(db.String(45))
@@ -140,6 +150,9 @@ class Address(db.Model):
     zip = db.Column(db.Integer)
     city = db.Column(db.String(45))
     email = db.Column(db.String(45))
+
+    challenge = db.relationship("Challenge")
+    user = db.relationship("User", back_populates="address")
 
     def __repr__(self):
         return str(self.id)
