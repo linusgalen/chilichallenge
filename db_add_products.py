c30b32c6dd0d36fd372faa8d671b431b1babddd6
Oscar Ek <oscek626@student.liu.se>
2017-02-24 10:48:41 +0100

    removed åäö

--- db_add_products.py
+++ db_add_products.py
@@ -4,24 +4,25 @@ from app.models import Product
 product_list=[
     {
         'name': 'Svag Röd Chili',
-        'description': 'Den här chilin kommer ifrån den mörkaste djungeln och har plockats med kärlek. Perfekt för nybörjaren',
+        'description': 'Den har chilin kommer ifran den morkaste djungeln och har plockats med karlek. Perfekt för nyborjaren',
         'imgurl':'http://www.publicdomainpictures.net/pictures/10000/velka/1-1248162429KMBk.jpg',
         'price' : 59
     },
     {
         'name': 'Chili el medio',
-        'description': 'Den här chilin kommer ifrån har plockats med kärlek och kommer ge en stabil hetta i munnen',
+        'description': 'Den har chilin kommer ifran har plockats med karlek och kommer ge en stabil hetta i munnen',
         'imgurl': 'https://upload.wikimedia.org/wikipedia/commons/9/98/Red_Pepper_-_on_white.jpg',
         'price': 69
     },
     {
         'name': 'Chili el diablo',
-        'description': 'Detta är inget för veklingar. Chili el diablo kommer ifrån El salvador och odlas av blinda munkar vilket gör den väldiigt exklusiv men ack så stark!',
+        'description': 'Detta ar inget för veklingar. Chili el diablo kommer ifran El salvador och odlas av blinda munkar vilket gor den valdiigt exklusiv men ack sa stark!',
         'imgurl': 'https://upload.wikimedia.org/wikipedia/commons/e/e6/Habanero_peper.jpg',
         'price': 199
     }
-
 ]
+
+
 
 for product in product_list:
 
