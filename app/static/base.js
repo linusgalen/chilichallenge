7df9ed6b32c65187219041129ff4583429241b9f
Linus <linga300@student.liu.se>
2017-04-03 11:13:41 +0200

    Removed carros jquery

--- app/static/base.js
+++ app/static/base.js
@@ -1,17 +1,17 @@
 
 
 $(document).ready(function() {
-    $("nav a, .scroll-arrow a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            var hash = this.hash;
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
+    // $("nav a, .scroll-arrow a").on('click', function (event) {
+    //     if (this.hash !== "") {
+    //         event.preventDefault();
+    //         var hash = this.hash;
+    //         $('html, body').animate({
+    //             scrollTop: $(hash).offset().top
+    //         }, 800, function () {
+    //             window.location.hash = hash;
+    //         });
+    //     }
+    // });
 
     $(".carousel").carousel({
     interval: 3000
